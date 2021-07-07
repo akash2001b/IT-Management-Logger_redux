542f6d39e26e05b6790f87155ee04ccfc7d14527
akash Roy Choudhury <arc.ar602@gmail.com>
2021-07-07 23:34:29 +0530

    redux add log method

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import { connect } from "react-redux";
+import { addLog } from "../../actions/logActions";
 import PropTypes from "prop-types";
 import M from "materialize-css/dist/js/materialize.min.js";
 
@@ -12,8 +13,18 @@ const AddLogModal = ({ addLog }) => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
+      
+      const newLog={
+        message,
+        attention,
+        tech,
+        date:new Date()
+      };
+      
+      addLog(newLog);
+      
       M.toast({ html: `Log added by ${tech}` });
-
+      
       // Clear Fields
       setMessage("");
       setTech("");
@@ -92,4 +103,4 @@ const modalStyle = {
   height: "75%",
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { apply } from "async";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialstate = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialstate,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
